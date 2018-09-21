d1cba64ddf9430556a0486554b4053d3e4fa340e
monin1 <neang.monin@yahoo.com>
2018-09-22 02:53:46 +0930

    update
    sign up

--- javascripts/click.js
+++ javascripts/click.js
@@ -58,3 +58,16 @@ $(document).ready(function() {
     }
   });
 });
+
+//New Folder
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
