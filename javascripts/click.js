bac61c22de9f28fcaefadcd8c02e86ef3cce2b57
monin1 <neang.monin@yahoo.com>
2018-09-22 03:09:02 +0930

    update
    sign up form

--- javascripts/click.js
+++ javascripts/click.js
@@ -59,15 +59,16 @@ $(document).ready(function() {
   });
 });
 
-//New Folder
+
+  //New Folder
 
 
-// Get the modal
-var modal = document.getElementById('id01');
+    // Get the modal
+    var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-}
